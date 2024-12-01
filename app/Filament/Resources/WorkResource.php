408401df7379d93cfa737c62cec9eca2c306201a
syahrinseth <syahrinsethservices@gmail.com>
2024-12-01 15:06:06 +0800

    added work experience module for admin panel

---
+++ app/Filament/Resources/WorkResource.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use Filament\Forms;
+use App\Models\Work;
+use Filament\Tables;
+use Filament\Forms\Form;
+use Filament\Tables\Table;
+use Filament\Resources\Resource;
+use Filament\Forms\Components\Section;
+use Illuminate\Database\Eloquent\Builder;
+use App\Filament\Resources\WorkResource\Pages;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\WorkResource\RelationManagers;
+
+class WorkResource extends Resource
+{
+    protected static ?string $model = Work::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Section::make('Basic Infomation')
+                    ->columns(2)
+                    ->collapsible()
+                    ->schema([
+                        Forms\Components\TextInput::make('title')
+                            ->required(),
+                        Forms\Components\RichEditor::make('description')
+                            ->columnSpanFull(),
+                        Forms\Components\Select::make('from')
+                            ->searchable()
+                            ->options([
+                                '2024' => '2024',
+                                '2023' => '2023',
+                            ]),
+                        Forms\Components\Select::make('to')
+                            ->searchable()
+                            ->options([
+                                'Present' => 'Present',
+                                '2024' => '2024',
+                                '2023' => '2023',
+                            ]),
+                    ]),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('title')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('from')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('to')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListWorks::route('/'),
+            'create' => Pages\CreateWork::route('/create'),
+            'edit' => Pages\EditWork::route('/{record}/edit'),
+        ];
+    }
+}
